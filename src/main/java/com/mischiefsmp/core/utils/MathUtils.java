346b93d64da596f56f941ad859e660782082d7d3
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-05 09:41:12 +0200

    Update MathUtils.java

--- src/main/java/com/mischiefsmp/core/utils/MathUtils.java
+++ src/main/java/com/mischiefsmp/core/utils/MathUtils.java
@@ -1,5 +1,7 @@
 package com.mischiefsmp.core.utils;
 
+import java.util.Random;
+
 public class MathUtils {
     public static boolean isInteger(String string) {
         try {
@@ -8,5 +10,36 @@ public class MathUtils {
         } catch (Exception exception) {
             return false;
         }
+    }
+
+    public static boolean isLong(String string) {
+        try {
+            Long.parseLong(string);
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
+    }
+
+    public static boolean isFloat(String string) {
+        try {
+            Float.parseFloat(string);
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
+    }
+
+    public static boolean isDouble(String string) {
+        try {
+            Double.parseDouble(string);
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
+    }
+
+    public static int randomRange(int min, int max) {
+        return new Random().nextInt(max + 1 - min) + min;
     }
 }
