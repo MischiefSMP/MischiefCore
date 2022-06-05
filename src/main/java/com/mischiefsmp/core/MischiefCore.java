d6c1713d65c71695144cc2d11767a24c49ea95fc
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-05 10:01:14 +0200

    Add getInstance method to MischiefCore

--- src/main/java/com/mischiefsmp/core/MischiefCore.java
+++ src/main/java/com/mischiefsmp/core/MischiefCore.java
@@ -3,7 +3,14 @@ package com.mischiefsmp.core;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MischiefCore extends JavaPlugin {
+    private static MischiefCore pluginInstance;
+
     @Override
     public void onEnable() {
+        pluginInstance = this;
+    }
+
+    public static MischiefCore getInstance() {
+        return pluginInstance;
     }
 }
