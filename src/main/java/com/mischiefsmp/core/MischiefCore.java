46be3b228cd1ae675a156b72c3692aabd63f42f3
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-05 09:28:10 +0200

    Caching loggers

--- src/main/java/com/mischiefsmp/core/MischiefCore.java
+++ src/main/java/com/mischiefsmp/core/MischiefCore.java
@@ -3,15 +3,24 @@ package com.mischiefsmp.core;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+
 public class MischiefCore extends JavaPlugin {
     private static final MCUtils mcUtils = new MCUtils();
+    private static final HashMap<Plugin, LogManager> loggers = new HashMap<>();
 
     @Override
     public void onEnable() {
     }
 
     public static ILogManager getLogManager(Plugin plugin) {
-        return new LogManager(plugin);
+        if(loggers.containsKey(plugin))
+            return loggers.get(plugin);
+
+        LogManager lm = new LogManager(plugin);
+        loggers.put(plugin, lm);
+
+        return lm;
     }
 
     public static IMCUtils getMCUtils() {
