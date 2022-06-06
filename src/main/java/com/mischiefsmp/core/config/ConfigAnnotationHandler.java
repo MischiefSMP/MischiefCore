470ba8ab83fa45bb73e0b75674e4a60a4eee120a
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-06 16:16:04 +0200

    Add optional indexes to update parameter

--- src/main/java/com/mischiefsmp/core/config/ConfigAnnotationHandler.java
+++ src/main/java/com/mischiefsmp/core/config/ConfigAnnotationHandler.java
@@ -1,18 +1,42 @@
 package com.mischiefsmp.core.config;
 
+import com.mischiefsmp.core.MischiefCore;
 import org.bukkit.configuration.ConfigurationSection;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 
 public class ConfigAnnotationHandler {
-    public void load(ConfigurationSection config, Object classToLoad) throws IllegalAccessException {
-        Class<?> clazz = classToLoad.getClass();
+    public void load(ConfigurationSection config, Object classToLoad, int... indexes) throws IllegalAccessException {
+        try {
 
-        for (Field field : clazz.getDeclaredFields()) {
-            field.setAccessible(true);
-            ConfigValue annotation = field.getAnnotation(ConfigValue.class);
-            if (annotation != null)
-                field.set(classToLoad, config.get(annotation.path()));
+            Class<?> clazz = classToLoad.getClass();
+
+            Field[] fields = clazz.getDeclaredFields();
+            for (int i = 0; i < fields.length; i++) {
+                Field field = fields[i];
+                field.setAccessible(true);
+                ConfigValue annotation = field.getAnnotation(ConfigValue.class);
+                if (annotation != null) {
+                    boolean doSet = false;
+
+                    if (indexes.length > 0) {
+                        for (int nr : indexes) {
+                            if (nr == i) {
+                                doSet = true;
+                                break;
+                            }
+                        }
+                    } else {
+                        doSet = true;
+                    }
+
+                    if (doSet)
+                        field.set(classToLoad, config.get(annotation.path()));
+                }
+            }
+        } catch(Exception exception) {
+            MischiefCore.getLogManager().logF("Error trying to load %s from class %s for indexes %s", config, classToLoad, Arrays.toString(indexes));
         }
     }
 }
