d5a9482c30fb7a50bfbeb18de20a5bf0ab561ace
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-08 12:36:41 +0200

    Implement lombok

--- src/main/java/com/mischiefsmp/core/MischiefCore.java
+++ src/main/java/com/mischiefsmp/core/MischiefCore.java
@@ -1,22 +1,17 @@
 package com.mischiefsmp.core;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MischiefCore extends JavaPlugin {
+    @Getter
     private static MischiefCore pluginInstance;
+    @Getter
     private static LogManager logManager;
 
     @Override
     public void onEnable() {
         pluginInstance = this;
         logManager = new LogManager(pluginInstance);
-    }
-
-    public static LogManager getLogManager() {
-        return logManager;
-    }
-
-    public static MischiefCore getInstance() {
-        return pluginInstance;
     }
 }
--- src/main/java/com/mischiefsmp/core/config/ConfigFile.java
+++ src/main/java/com/mischiefsmp/core/config/ConfigFile.java
@@ -1,27 +1,19 @@
 package com.mischiefsmp.core.config;
 
+import lombok.Getter;
 import org.bukkit.plugin.Plugin;
 
 public class ConfigFile {
+    @Getter
     private final Plugin plugin;
+    @Getter
     private final String localPath;
+    @Getter
     private final String jarPath;
 
     public ConfigFile(Plugin plugin, String localPath, String jarPath) {
         this.plugin = plugin;
         this.localPath = localPath;
         this.jarPath = jarPath;
-    }
-
-    public Plugin getPlugin() {
-        return plugin;
-    }
-
-    public String getLocalPath() {
-        return localPath;
-    }
-
-    public String getJarPath() {
-        return jarPath;
     }
 }
