0696c2421369137d004d9c3437be269d95a303e8
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-09 10:02:17 +0200

    Add roundUtils

--- src/main/java/com/mischiefsmp/core/utils/MathUtils.java
+++ src/main/java/com/mischiefsmp/core/utils/MathUtils.java
@@ -42,4 +42,13 @@ public class MathUtils {
     public static int randomRange(int min, int max) {
         return new Random().nextInt(max + 1 - min) + min;
     }
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
 }
