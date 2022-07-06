50cb6719cdb009b548b4b1da5adc1e714ac030b6
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-07-06 11:39:07 +0200

    Add utility method for arguments in tab autocomplete

--- src/main/java/com/mischiefsmp/core/utils/Utils.java
+++ src/main/java/com/mischiefsmp/core/utils/Utils.java
@@ -5,6 +5,8 @@ import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.hover.content.Text;
 
+import java.util.List;
+
 public class Utils {
     public static TextComponent getHoverAndCMDText(String text, String hover, String command) {
         TextComponent txt = new TextComponent(text);
@@ -13,6 +15,14 @@ public class Utils {
         if(command != null)
             txt.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, command));
         return txt;
+    }
+
+    public static boolean startsWithIgnoreCase(String str1, String str2) {
+        return str1.toLowerCase().startsWith(str2.toLowerCase());
+    }
+
+    public static void removeTabArguments(String[] args, List<String> tabArguments) {
+        tabArguments.removeIf(str -> !Utils.startsWithIgnoreCase(str, args[args.length - 1]));
     }
 
     public static boolean contains(int[] list, int nr) {
