993b5977d187a758ecaf227ce9858ef848473d33
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-07-26 10:59:16 +0200

    Added mischief plugin indexing + better load functions

--- src/main/java/com/mischiefsmp/core/MischiefCore.java
+++ src/main/java/com/mischiefsmp/core/MischiefCore.java
@@ -1,16 +1,55 @@
 package com.mischiefsmp.core;
 
 import com.mischiefsmp.core.utils.FileUtils;
+import com.mischiefsmp.core.utils.LogManager;
+import org.bukkit.plugin.Plugin;
+
+import java.util.HashMap;
 
 public class MischiefCore extends MischiefPlugin {
+    private static final HashMap<Class<?>, MischiefPlugin> mischiefPlugins = new HashMap<>();
+    private static final HashMap<String, MischiefPlugin> mischiefPluginsByName = new HashMap<>();
 
     @Override
-    public void onEnable() {
+    public void onLoad() {
         FileUtils.init(this);
     }
 
     @Override
-    public void onDisable() {
+    public void onFullLoad() {
+        LogManager lm = getLogManager();
+        lm.log("Indexing Mischief plugins...");
+        for(Plugin pl : getServer().getPluginManager().getPlugins()) {
+            if(pl instanceof MischiefPlugin plugin) {
+                lm.logF("Indexing %s...", plugin.getName());
+                mischiefPlugins.put(pl.getClass(), plugin);
+                mischiefPluginsByName.put(pl.getName(), plugin);
+            }
+        }
+        lm.log("Done!");
+    }
 
+    @Override
+    public void onUnload() {
+
+    }
+
+    public static LogManager getLogManager(Class<?> pluginClass) {
+        MischiefPlugin plugin = getMischiefPlugin(pluginClass);
+        if(plugin != null)
+            return plugin.getLogManager();
+        return null;
+    }
+
+    public static MischiefPlugin getMischiefPlugin(Class<?> pluginClass) {
+        if(mischiefPlugins.containsKey(pluginClass))
+            return mischiefPlugins.get(pluginClass);
+        return null;
+    }
+
+    public static MischiefPlugin getMischiefPlugin(String name) {
+        if(mischiefPluginsByName.containsKey(name))
+            return mischiefPluginsByName.get(name);
+        return null;
     }
 }
--- src/main/java/com/mischiefsmp/core/MischiefPlugin.java
+++ src/main/java/com/mischiefsmp/core/MischiefPlugin.java
@@ -4,6 +4,7 @@ import com.mischiefsmp.core.lang.LangManager;
 import com.mischiefsmp.core.utils.Function;
 import com.mischiefsmp.core.utils.LogManager;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -17,10 +18,21 @@ public abstract class MischiefPlugin extends JavaPlugin {
     public MischiefPlugin() {
         logManager = LogManager.getLogManager(this);
         langManager = new LangManager(this);
+        //This is run once the server is fully started
     }
 
-    public abstract void onEnable();
-    public abstract void onDisable();
+    public void onEnable() {
+        onLoad();
+        getServer().getScheduler().scheduleSyncDelayedTask(this, this::onFullLoad);
+    }
+
+    public void onDisable() {
+        onUnload();
+    }
+
+    public abstract void onLoad();
+    public abstract void onFullLoad();
+    public abstract void onUnload();
 
     public void setReloadFunction(Function function) {
         reloadFunction = function;
