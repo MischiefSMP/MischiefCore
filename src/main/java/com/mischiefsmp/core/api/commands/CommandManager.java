ff7e2671bf8cc32956d8f610e927ff1b7a8e9d19
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-08-04 10:11:06 +0200

    Implement subCommand logic

--- src/main/java/com/mischiefsmp/core/api/commands/CommandManager.java
+++ src/main/java/com/mischiefsmp/core/api/commands/CommandManager.java
@@ -1,18 +1,25 @@
 package com.mischiefsmp.core.api.commands;
 
 import com.mischiefsmp.core.api.MischiefPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandMap;
 import org.bukkit.command.PluginCommand;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.logging.Level;
 
 public class CommandManager {
     public static void registerCommand(@NotNull MischiefPlugin plugin, @NotNull MischiefCommand command) {
-        PluginCommand cmd = plugin.getCommand(command.getAlias());
-        if(cmd == null) {
-            plugin.getLogManager().warn("Could not register command %s", command.getAlias());
-            return;
+        try {
+            Field bukkitCommandMap = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+            bukkitCommandMap.setAccessible(true);
+            CommandMap commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
+            commandMap.register(command.getLabel(), command.getCommand());
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            plugin.getLogManager().warn("Could not register command %s!", command.getLabel());
+            plugin.getLogManager().warn(e.getMessage());
         }
-        cmd.setExecutor(command.getExecutor());
     }
 }
--- src/main/java/com/mischiefsmp/core/api/commands/MischiefCommand.java
+++ src/main/java/com/mischiefsmp/core/api/commands/MischiefCommand.java
@@ -4,35 +4,74 @@ import com.mischiefsmp.core.MischiefCore;
 import com.mischiefsmp.core.api.MischiefPlugin;
 import lombok.Getter;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
-public abstract class MischiefCommand {
+import java.util.HashMap;
+
+public abstract class MischiefCommand{
     private final MischiefCommand instance;
 
     @Getter
-    private final CommandExecutor executor;
+    private final Command command;
+
+    private final HashMap<String, MischiefCommand> subCommands = new HashMap<>();
 
     public MischiefCommand(MischiefPlugin plugin) {
         instance = this;
-        executor = (sender, command, label, args) -> {
-            String message = null;
-            switch(instance.onCommand(sender, command, plugin, label, args)) {
-                case NO_PERMISSION -> message = "cmd-no-perm";
-                case MISSING_ARGUMENTS -> message = "cmd-missing-arg";
-                case BAD_ARGUMENTS -> message = "cmd-bad-arg";
-                case SERVER_ERROR -> message = "cmd-server-error";
+        command = new Command(getLabel()) {
+            @Override
+            public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
+                String message = null;
+
+                //Check for sub commands
+                if(args.length != 0) {
+                    for(String key : subCommands.keySet()) {
+                        MischiefCommand cmd = subCommands.get(key);
+                        if(cmd != null && cmd.isSameLabelAlias(args[0])) {
+                            //We found a sub command! Yay!
+                            int newLength = args.length - 1;
+                            String[] newArgs = new String[newLength];
+                            System.arraycopy(args, 1, newArgs, 0, newLength);
+                            cmd.getCommand().execute(sender, commandLabel, newArgs);
+                            return true;
+                        }
+                    }
+                }
+
+                switch(instance.onCommand(sender, command, plugin, commandLabel, args)) {
+                    case NO_PERMISSION -> message = "cmd-no-perm";
+                    case MISSING_ARGUMENTS -> message = "cmd-missing-arg";
+                    case BAD_ARGUMENTS -> message = "cmd-bad-arg";
+                    case SERVER_ERROR -> message = "cmd-server-error";
+                }
+
+                if(message != null)
+                    MischiefCore.getCore().getLangManager().sendString(sender, message);
+                return true;
             }
-
-            if(message != null)
-                MischiefCore.getCore().getLangManager().sendString(sender, message);
-
-            return true;
         };
     }
 
-    public abstract String getAlias();
+    public abstract String getLabel();
+    public abstract String[] getAliases();
+
+    public boolean isSameLabelAlias(String label) {
+        if(getLabel().equals(label))
+            return true;
+
+        if(getAliases() == null)
+            return false;
+
+        for(String alias : getAliases())
+            if(alias.equals(label))
+                return true;
+        return false;
+    }
 
     public abstract CommandResult onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull MischiefPlugin plugin, @NotNull String label, @NotNull String[] args);
+
+    public void addSubCommand(MischiefCommand command) {
+        subCommands.put(command.getLabel(), command);
+    }
 }
