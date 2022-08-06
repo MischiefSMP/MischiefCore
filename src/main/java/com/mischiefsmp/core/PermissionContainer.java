675ac9f8bf28453668cb3959657629b3ae175639
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-08-06 13:19:28 +0200

    Update PermissionContainer.java

--- src/main/java/com/mischiefsmp/core/PermissionContainer.java
+++ src/main/java/com/mischiefsmp/core/PermissionContainer.java
@@ -4,6 +4,10 @@ import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 
 public class PermissionContainer {
+    //View help command
     public static Permission CORE_HELP = new Permission("core.help", PermissionDefault.OP);
+    //View plugins
     public static Permission CORE_PLUGINS = new Permission("core.plugins", PermissionDefault.FALSE);
+    public static Permission CORE_PLUGINS_UNINSTALL = new Permission("core.plugins.uninstall", PermissionDefault.FALSE);
+    public static Permission CORE_PLUGINS_INSTALL = new Permission("core.plugins.install", PermissionDefault.FALSE);
 }
